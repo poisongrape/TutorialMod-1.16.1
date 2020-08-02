11adf22d058079da94626c72672ff85be9b8d33e
poisongrape <poisongrape111@gmail.com>
2020-08-02 11:59:00 -0700

    Update mod id and toml fields.

--- src/main/java/com/poisongrape/tutorial/Tutorial.java
+++ src/main/java/com/poisongrape/tutorial/Tutorial.java
@@ -9,7 +9,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("tutorial")
 public class Tutorial
 {
     private static final Logger LOGGER = LogManager.getLogger();
